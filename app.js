ef8711153942e61c6432dc4ae5f13ad869d2febe
LS <manunited_22@sina.com>
2017-02-23 15:26:52 +0800

    增加koa2路由

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const App = require('koa'),
   app = new App(),
   koa = require('koa-router')(),
   json = require('koa-json'),
-  logger = require('koa-logger'); // 引入各种依赖
+  logger = require('koa-logger'), // 引入各种依赖
+  auth = require('./server/routes/auth.js');
 
 app.use(require('koa-bodyparser')());
 app.use(json());
@@ -10,7 +11,7 @@ app.use(logger());
 
 app.use(async (ctx, next) => {
   let start = new Date;
-  await next;
+  await next();
   let ms = new Date - start;
   console.log('%s %s - %s', this.method, this.url, ms); // 显示执行的时间
 });
@@ -18,6 +19,12 @@ app.use(async (ctx, next) => {
 app.on('error', function(err, ctx){
   console.log('server error', err);
 });
+
+app.use(auth.routes());
+
+// koa.use('/auth', auth.routes());
+
+// app.use(koa.routes());
 
 app.listen(8889,() => {
   console.log('Koa is listening in 8889');
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,16 +1,17 @@
 let User = require('../models/users');
 
 let user = new User("liu1", "123456");
-// user.login();
-// console.log(user.login());
 
-(async () => {
+async function getUserInfo(){
   let backData = await user.login();
   console.log(backData);
   if(backData.password == user.password){
     console.log('成功');
   }
-  // console.log(backData);
-})()
+}
+
+
+// module.exports = getUserInfo;
+module.exports = getUserInfo;
 
 
