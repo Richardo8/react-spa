49374eef1f65d6d8f3df553fbc43829b4378d82e
LS <manunited_22@sina.com>
2017-02-23 16:42:19 +0800

    将ES6的类和async相结合，简化代码

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,8 +2,8 @@ let User = require('../models/users');
 
 
 async function getUserInfo(name){
-  let user = new User();
-  return await user.login(name);
+  // let user = new User();
+  return await User.login(name);
 }
 
 module.exports = getUserInfo;
--- server/models/users.js
+++ server/models/users.js
@@ -30,22 +30,29 @@ class User {
     })
   }
 
-  login(name){
-    // let data = {
-    //   name: this.name,
-    //   password: this.password
-    // };
-    return async function get() {
+  // login(name){
+  //   // let data = {
+  //   //   name: this.name,
+  //   //   password: this.password
+  //   // };
+  //   return async function get() {
+  //     return await userModel.findOne({'name': name});
+  //   }();
+  //   // return (async () => {
+  //   //   return await userModel.findOne({'name': data.name});
+  //   // })();
+  //   /*
+  //   * 使用async返回数据。
+  //   * 不想在model中设计逻辑判断的话，需要将数据返回到controller中，所以需要将获得的数据return，以上两种写法均可。至此，不需要回调函数。
+  //   * node中运行时别忘了加 --harmony-async-await*/
+  // }
+
+  static async login(name){
       return await userModel.findOne({'name': name});
-    }();
-    // return (async () => {
-    //   return await userModel.findOne({'name': data.name});
-    // })();
-    /*
-    * 使用async返回数据。
-    * 不想在model中设计逻辑判断的话，需要将数据返回到controller中，所以需要将获得的数据return，以上两种写法均可。至此，不需要回调函数。
-    * node中运行时别忘了加 --harmony-async-await*/
   }
+  /*
+  * 类的静态方法只有类可以调用，所以在models中的user.js不需要实例化
+  * 以上两种方法均可以使用，第二种写法更简单，但是要注意如上所提*/
 }
 
 module.exports = User;
