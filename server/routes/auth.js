b9e7f182aad470b742eb9d5f775260cb936469f3
LS <manunited_22@sina.com>
2017-02-23 16:23:37 +0800

    修改方法实现url传入参数，在数据库中搜索数据

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,11 +3,12 @@ const getUserInfo = require('../controllers/user'),
 
 router.get('/user/:name', async (ctx, next) => {
   let name = ctx.params.name;
-  let data = await getUserInfo(name);
-  console.log(data);
-  ctx.body = data;
+  ctx.body = await getUserInfo(name);
   await next();
 });
+
+/*
+* 在路由中使用ctx.params.name获取url中相关数据（即冒号后边的数据），搜索完数据之后使用ctx.bod = 即可返回查询到的数据*/
 
 module.exports = router;
 
